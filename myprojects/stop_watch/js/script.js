21e971ecaa7ddf44ff21ee4003cebbddafde9d73
mahan <142781243+mahan-07@users.noreply.github.com>
2023-12-16 13:08:09 +0330

    update
    new update

--- myprojects/stop_watch/js/script.js
+++ myprojects/stop_watch/js/script.js
@@ -8,9 +8,10 @@ function Stop() {
   stopp = 1;
   anim = 0;
   normal = 2;
+  newday= 0;
   if (day == 1 && momo == 1) {
     document.querySelector("body").style =
-      "background-color: black; color: gold;";
+      "color: gold;";
     document.getElementById("screen").style =
       "background-color: black; border: 2px solid red;";
     document.getElementById("sadom").style =
@@ -20,6 +21,7 @@ function Stop() {
     document.getElementById("Reset").style =
       "background-color: black; color: gold; border: 2px solid gold";
     document.getElementById("Start").innerHTML = "Start";
+    document.querySelector("body").style.backgroundColor="black"
   } else {
     document.getElementById("Start").style.backgroundColor =
       "rgb(162, 206, 95)";
@@ -88,6 +90,7 @@ function Start() {
 
 function Reset() {
   normal = 0;
+  newday = 1;
   document.getElementById("sadom").innerHTML = 0;
   document.getElementById("sec").innerHTML = 0;
   document.getElementById("min").innerHTML = 0;
@@ -96,7 +99,7 @@ function Reset() {
 }
 
 function sadom() {
-  alert("صدم ثانیه ، میکروثانیه || 0.01");
+  alert("Microsecond || 0.01");
 }
 
 /// حالت شب
@@ -106,7 +109,7 @@ function mode() {
   if (day == 0) {
     document.getElementById("screen").style = "background-color: black;";
     document.querySelector("body").style =
-      "background-color: black; color: gold;";
+      "color: gold;";
 
     if (normal == 0) {
     } else {
@@ -121,12 +124,15 @@ function mode() {
       }
     }
 
+    document.getElementById("mode").style = "background-image: url(pic/sun.svg); background-color: transparent ; filter: invert(69%) sepia(98%) saturate(587%) hue-rotate(358deg) brightness(105%) contrast(108%);";
+    document.getElementById("me").style.color = "white";
     document.getElementById("sadom").style =
-      "background-color: black; color: gold; border: 2px solid gold";
+      "background-color: black; color: gold; border: 2px solid gold;";
     document.getElementById("Start").style =
       "background-color: black; color: gold; border: 2px solid gold";
     document.getElementById("Reset").style =
       "background-color: black; color: gold; border: 2px solid gold";
+    document.querySelector("body").style.backgroundColor="black"
     day = 1;
     momo = 1;
   } else {
@@ -146,6 +152,8 @@ function mode() {
       }
     }
 
+    document.getElementById("mode").style = "background-image: ; background-color: ; filter: ;";
+    document.getElementById("me").style.color = ""
     document.getElementById("sadom").style =
       "background-color: ; color: ; border: ";
     document.getElementById("Start").style =
@@ -160,3 +168,62 @@ function mode() {
     momo = 0;
   }
 }
+
+// get the h1 element by its id
+var h1 = document.getElementById ("me");
+// add a click event listener to the h1 element
+h1.addEventListener ("click", function () {
+  // create a temporary input element
+  var input = document.createElement ("input");
+  // append it to the document body
+  document.body.appendChild (input);
+  // set its value to the text of the h1 element
+  input.value = h1.textContent;
+  // select its value
+  input.select ();
+  // copy it to the clipboard
+  document.execCommand ("copy");
+  // remove it from the document body
+  document.body.removeChild (input);
+  // show an alert message
+
+
+  // Get the browser language
+var userLang = navigator.language || navigator.userLanguage;
+
+// Define a list of supported languages
+var langs = ['fa-IR'];
+
+// Check if the user language is in the list
+if (langs.includes(userLang)) {
+  // If yes, display an alert box with a message in that language
+  alert("متن در کلیپ بورد کپی شد!");
+} else {
+  // If no, display an alert box with a message in English
+  alert("The text has been copied to the clipboard");
+}
+});
+
+// clock
+setInterval(()=>{
+  const time = document.querySelector(".display #time");
+  let date = new Date();
+  let hours = date.getHours();
+  let minutes = date.getMinutes();
+  let seconds = date.getSeconds();
+  let day_night = "AM";
+  if(hours > 12){
+    day_night = "PM";
+    hours = hours - 12;
+  }
+  if(seconds < 10){
+    seconds = "0" + seconds;
+  }
+  if(minutes < 10){
+    minutes = "0" + minutes;
+  }
+  if(hours < 10){
+    hours = "0" + hours;
+  }
+  time.textContent = hours + ":" + minutes + ":" + seconds + " "+ day_night;
+});
